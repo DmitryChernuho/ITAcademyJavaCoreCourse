0ee5958939cb875b73bac46e71a9423143e8a325
Dmitry Chernuho <d.chernuho@gmail.com>
2022-12-24 15:31:14 +0300

    Tasks of fourth lesson are done.

---
+++ src/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    static final int UPPER_LIMIT = 10;
+
+    public static void main(String[] args) {
+        Tasks.taskNumberOne(UPPER_LIMIT);
+        Tasks.taskNumberTwo(UPPER_LIMIT);
+        Tasks.taskThree(UPPER_LIMIT);
+        Tasks.taskNumberFour(UPPER_LIMIT);
+    }
+}
