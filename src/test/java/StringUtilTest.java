6f2e00d46ff1dd378b35888372fefdde4eaae58f
Dmitry Chernuho <d.chernuho@gmail.com>
2023-01-18 04:19:12 -0800

    All methods for StringUtil was realized

---
+++ src/test/java/StringUtilTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class StringUtilTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/tests_data/concatTwoValues.csv", numLinesToSkip = 1)
+    private void concatTwoValuesTest(ArgumentsAccessor argumentsAccessor) {
+//        argumentsAccessor.
+//        String actualResult = StringUtil.concatTwoValues(firstValue, secondValue);
+//        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
